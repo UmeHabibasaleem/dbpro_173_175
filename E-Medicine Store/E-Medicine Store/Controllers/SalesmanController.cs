ee5eca170f7aeb6e08abbe6aea71b2d067d587a1
hinayaqub <hina54796@gmail.com>
2019-04-28 18:49:28 +0500

    New Salesman

--- E-Medicine Store/E-Medicine Store/Controllers/SalesmanController.cs
+++ E-Medicine Store/E-Medicine Store/Controllers/SalesmanController.cs
@@ -6,7 +6,9 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using  E_Medicine_Store.Models;
+using E_Medicine_Store.Models;
+using System.Data.Entity;
+
 namespace E_Medicine_Store.Controllers
 {
     public class SalesmanController : Controller
@@ -15,13 +17,52 @@ namespace E_Medicine_Store.Controllers
         // GET: Salesman
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                List<Salesman> list = db.Salesmen.ToList();
+                List<SalesmanViewModel> viewList = new List<SalesmanViewModel>();
+                foreach (Salesman s in list)
+                {
+                    SalesmanViewModel obj = new SalesmanViewModel();
+                    obj.SalesmanID = s.SalesmanID;
+                    obj.Name= s.Name;
+                    obj.CNIC = s.CNIC;
+                    obj.Phone = s.Phone;
+                    obj.CompanyID = s.CompanyID;
+                    obj.StockID = s.StockID;
+                    viewList.Add(obj);
+                }
+                return View(viewList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+          
         }
 
         // GET: Salesman/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
-            return View();
+            SalesmanViewModel obj = new SalesmanViewModel();
+            Salesman s = db.Salesmen.Find(id);
+            if (s == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                obj.SalesmanID = s.SalesmanID;
+                obj.Name = s.Name;
+                obj.CNIC = s.CNIC;
+                obj.Phone = s.Phone;
+                obj.CompanyID = s.CompanyID;
+                obj.StockID = s.StockID;
+
+               
+            }
+            return View(obj);
+          
         }
 
         // GET: Salesman/Create
@@ -42,52 +83,105 @@ namespace E_Medicine_Store.Controllers
 
         // POST: Salesman/Create
         [HttpPost]
-        public ActionResult Create(SalesmanViewModel sales)
+        public ActionResult Create(SalesmanViewModel obj)
         {
-          
+            try
+            {
+                Salesman s = new Salesman();
+                s.Name = obj.Name;
+                s.CNIC = obj.CNIC;
+                s.Phone = obj.Phone;
+                s.CompanyID = obj.CompanyID;
+                s.StockID = obj.StockID;
+
+                db.Salesmen.Add(s);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
                 return View();
-            
+            }
+
+
         }
 
         // GET: Salesman/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+            SalesmanViewModel obj = new SalesmanViewModel();
+            Salesman s = db.Salesmen.Find(id);
+            if (s == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                obj.SalesmanID = s.SalesmanID;
+                obj.Name = s.Name;
+                obj.CNIC = s.CNIC;
+                obj.Phone = s.Phone;
+                obj.CompanyID = s.CompanyID;
+                obj.StockID = s.StockID;
+            }
+            return View(obj);
         }
 
         // POST: Salesman/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int? id, SalesmanViewModel obj)
         {
             try
             {
-                // TODO: Add update logic here
-
+                //db.Salesmen.Find(id).SalesmanID = obj.SalesmanID;
+                db.Salesmen.Find(id).Name = obj.Name;
+                db.Salesmen.Find(id).Phone = obj.Phone;
+                db.Salesmen.Find(id).CompanyID = obj.CompanyID;
+                db.Salesmen.Find(id).StockID = obj.StockID;
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
             {
                 return View();
             }
+            
         }
 
         // GET: Salesman/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
-            return View();
+            SalesmanViewModel obj = new SalesmanViewModel();
+            Salesman s = db.Salesmen.Find(id);
+            if (s == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                obj.SalesmanID = s.SalesmanID;
+                obj.Name = s.Name;
+                obj.Phone = s.Phone;
+                obj.CompanyID = s.CompanyID;
+                obj.StockID = s.StockID;
+            }
+            return View(obj);
+            
         }
 
         // POST: Salesman/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Delete(int? id, SalesmanViewModel obj)
         {
             try
             {
-                // TODO: Add delete logic here
-
+                var v = db.Salesmen.Where(a => a.SalesmanID == id).First();
+                db.Entry(v).State = EntityState.Deleted;
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
                 return View();
             }
