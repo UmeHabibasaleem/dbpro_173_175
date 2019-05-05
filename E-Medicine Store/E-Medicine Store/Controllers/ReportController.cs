9c8ded366a155d829636bd1eca400408b04e4d35
UmeHabibasaleem <umehabiba.cs@gmail.com>
2019-05-04 22:23:22 -0700

    Report Commit

--- E-Medicine Store/E-Medicine Store/Controllers/ReportController.cs
+++ E-Medicine Store/E-Medicine Store/Controllers/ReportController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using E_Medicine_Store.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,77 @@ namespace E_Medicine_Store.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult MYReport()
+        {
+          var id = (int)Session["OwnerId"];
+            using (var context = new DB5Entities5())
+            {
+                var courses = context.CompanyRecord(id).ToList();
+                CompanyRecordrpt rpt = new CompanyRecordrpt();
+                rpt.Load();
+                rpt.SetDataSource(courses);
+                Stream s = rpt.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                return File(s, "application/pdf");
+            }
+        }
+        public ActionResult Attendence_Per_day()
+        {
+            var id = (int)Session["OwnerId"];
+            DateTime Day = Convert.ToDateTime(Request["date"].ToString());
+            using (var context = new DB5Entities5())
+            {
+                var courses = context.Attendence_Per_day(Day,id).ToList();
+                AttendencePerDayReport rpt = new AttendencePerDayReport();
+                rpt.Load();
+                rpt.SetDataSource(courses);
+                Stream s = rpt.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                return File(s, "application/pdf");
+            }
+       }
+        public ActionResult Attendence_Per_Month()
+        {
+            int Year = Convert.ToInt32(Request["MyYear"].ToString());
+            int Month = Convert.ToInt32(Request["MyMonth"].ToString());
+            var id = (int)Session["OwnerId"];
+            using (var context = new DB5Entities5())
+            {
+                var Att = context.Attendence_Per_Month(Month,Year,id).ToList();
+                AttendancePerMonth rpt = new AttendancePerMonth();
+                rpt.Load();
+                rpt.SetDataSource(Att);
+                Stream s = rpt.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                return File(s, "application/pdf");
+            }
+        }
+        public ActionResult Medicine_sold_per_day()
+        {
+            var id = (int)Session["OwnerId"];
+            DateTime Day = Convert.ToDateTime(Request["date"].ToString());
+            using (var context = new DB5Entities5())
+            {
+                var courses = context.Medicine_sold_per_DAy(Day, id).ToList();
+                MedicinePerDayReport rpt = new MedicinePerDayReport();
+                rpt.Load();
+                rpt.SetDataSource(courses);
+                Stream s = rpt.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                return File(s, "application/pdf");
+            }
+        }
+        public ActionResult Medicine_sold_per_Month()
+        {
+            int Year = Convert.ToInt32(Request["MyYear"].ToString());
+            int Month = Convert.ToInt32(Request["MyMonth"].ToString());
+            var id = (int)Session["OwnerId"];
+            using (var context = new DB5Entities5())
+            {
+                var Att = context.Medicine_sold_per_Month(Year, Month, id).ToList();
+                MedicineSoldPerMonth rpt = new MedicineSoldPerMonth();
+                rpt.Load();
+                rpt.SetDataSource(Att);
+                Stream s = rpt.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                return File(s, "application/pdf");
+            }
         }
     }
 }
